4d5258b5b66fdae45f91bb4c0ad5bd0aa071cc35
msaimam98 <51413435+msaimam98@users.noreply.github.com>
2019-07-25 14:33:55 -0400

    Update dynamic_task_creation.py

--- Example_files/dynamic_task_creation.py
+++ Example_files/dynamic_task_creation.py
@@ -48,47 +48,16 @@ OFF_SALES = 40
 with open('/usr/local/airflow/dags/format.json') as g:
     data = json.load(g)
 
-
-# def mapping(dict, dag1):
-#
-#     if not dict['ttl']:
-#         t1 = DockerOperator(
-#             task_id=dict['task_id'],
-#             dag=dag1)
-#         return t1
-#     else:
-#         list_to_edit = []
-#         for task in dict['ttl']:
-#             list_to_edit.extend(mapping(task, dag))
-#         t = DockerOperator(
-#             task_id=dict['task_id'],
-#             dag=dag1)
-#         final = [t]
-#         final.extend(list_to_edit)
-#         for i in range(len(list_to_edit)):
-#             t.set_upstream(list_to_edit[i])
-#         return final
-
 def mapping(dict, dag1):
 
     if not dict['ttl']:
-        if bool(dict["eval_command"]) == True:
-            t1 = DockerOperator(
-                task_id=dict['task_id'],
-                image=dict['image'],
-                command=exec(dict['command']),
-                xcom_push=bool(dict['xcom_push']),
-                dag=dag1)
-            return [t1]
-        else:
-            t1 = DockerOperator(
-                task_id=dict['task_id'],
-                image=dict['image'],
-                command=dict['command'],
-                xcom_push=bool(dict['xcom_push']),
-                dag=dag1)
-            return [t1]
-
+      t1 = DockerOperator(
+            task_id=dict['task_id'],
+            image=dict['image'],
+            command=eval(dict['command']),
+            xcom_push=bool(dict['xcom_push']),
+            dag=dag1)
+        return [t1]
     else:
         list_to_edit = []
         for task in dict['ttl']:
@@ -107,52 +76,3 @@ def mapping(dict, dag1):
 
 return1 = mapping(data, dag)
 print(return1)
-
-# if __name__ == "__main__":
-#     print(data)
-#     return1 = mapping(data, dag)
-#     print(return1)
-
-
-# let the function print the json format to see the right input
-
-# testing is very important - test as you go
-
-# first without airflow - parsing is priority
-
-# generate memory objects
-
-
-# def mapping(dict):
-#
-#
-#     if not dict['ttl']:
-#         newnew = []
-#         for key in dict.keys():
-#             if key is not 'ttl':
-#                 if key is task or fegerg or e rg or  reg:
-#                 newnew.append((key, dict[key])
-#         t1 = DockerOperator(
-#             task_id=dict['task_id'],
-#             newnew[1][0] = newnew[1][1],
-#             newnew[2][0] = newnew[2][1],
-#             newnew[3][0] = newnew[3][1],
-#             newnew[4][0] = newnew[4][1])
-#         return t1
-#     else:
-#         list_to_edit = []
-#         for task in dict['ttl']:
-#             list_to_edit.append(mapping(task))
-#         list_to = []
-#         for key in dict.keys():
-#             if key is not dict['ttl']:
-#                 list_to.append((key, dict[key])
-#         t = DockerOperator(
-#             newnew[0][0]=newnew[0][1],
-#             newnew[1][0] = newnew[1][1],
-#             newnew[2][0] = newnew[2][1],
-#             newnew[3][0] = newnew[3][1],
-#             newnew[4][0] = newnew[4][1])
-#         strr =
-#         for i in range(len(list_to_edit))):
-#
